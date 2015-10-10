43ef3a9eb7d41ec7db8676395385f59aebd77444
Alexander Wirz <alex.wirz@gmail.com>
2015-10-10 17:01:42 +0200

    When file from watched directory changes
    given a file exists and it's path is watched when the file changes
    then subscribers are notified.

---
+++ blitz/Blitz.Facts/.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Blitz.Facts
+{
+    internal class WatchedPath
+    {
+        private FileSystemWatcher _pathWatcher;
+        private IList<Action> _onChagedHandlers = new List<Action>();
+
+        public WatchedPath(string tempDirectoryPath)
+        {
+            _pathWatcher = new FileSystemWatcher(tempDirectoryPath);
+            _pathWatcher.NotifyFilter =
+                            NotifyFilters.LastAccess |
+                            NotifyFilters.LastWrite |
+                            NotifyFilters.FileName;
+            _pathWatcher.Changed += WatchedPathContentChanged;
+        }
+
+        public void StartWatching()
+        {
+            _pathWatcher.EnableRaisingEvents = true;
+        }
+
+        private void WatchedPathContentChanged(object sender, FileSystemEventArgs e)
+        {
+            foreach (var handler in _onChagedHandlers)
+            {
+                handler();
+            }
+        }
+
+        internal void OnFileChanged(Action onChanged)
+        {
+            _onChagedHandlers.Add(onChanged);
+        }
+    }
+}
---
+++ blitz/Blitz.Facts/WatchingPaths.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Xbehave;
+using Xunit;
+
+namespace Blitz.Facts
+{
+    public class WatchingPaths : IDisposable
+    {
+        private string _tempDirectoryPath;
+        private string _watchedFilePath;
+
+        public WatchingPaths()
+        {
+            _tempDirectoryPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(_tempDirectoryPath);
+            _watchedFilePath = Path.Combine(_tempDirectoryPath, "test.txt");
+        }
+
+        [Scenario]
+        public void when_file_from_watched_directory_changes()
+        {
+            WatchedPath watchedPath = null;
+            bool subscriberNotified = false;
+            "given a file exists"
+                .f(() => { File.AppendAllText(_watchedFilePath, "foo"); });
+
+            "and it's path is watched"
+                .f(() => {
+                    watchedPath = new WatchedPath(
+                        Path.GetDirectoryName(_watchedFilePath));
+                    watchedPath.OnFileChanged(
+                        () => { subscriberNotified = true; });
+                    watchedPath.StartWatching();
+                });
+
+            "when the file changes"
+                .f(() => { File.AppendAllText(_watchedFilePath, "bar"); });
+
+            "then subscribers are notified"
+                .f(() => { Assert.True(subscriberNotified); });
+        }
+
+        public void Dispose()
+        {
+            try { Directory.Delete(_tempDirectoryPath, true); }
+            catch { }
+        }
+    }
+}
